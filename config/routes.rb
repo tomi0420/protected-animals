cf7ba504145a675953ebc7580e386d4de2be5602
tomi0420 <kohei.yoritomi@di-v.co.jp>
2021-05-08 19:31:13 +0900

    ウィザード形式で新規登録実装（users,user_addresses)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }  
+  devise_scope :user do
+    get 'user/addresses', to: 'users/registrations#new_user_address'
+    post 'user/addresses', to: 'users/registrations#create_user_address'
+  end
   root to: 'animals#index'
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_074310) do
+ActiveRecord::Schema.define(version: 2021_05_07_084231) do
+
+  create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_addresses_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -29,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_05_07_074310) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_addresses", "users"
 end
