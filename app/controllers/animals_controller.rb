40fbe35c56f5ac238cb00004b33ae4a12394a4c5
tomi0420 <kohei.yoritomi@di-v.co.jp>
2021-05-20 00:11:26 +0900

    showアクションの定義

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,5 @@
 class AnimalsController < ApplicationController
+  before_action :set_animal, only: :show
 
   def index
     @animals = Animal.all                   #保護団体を登録後、N＋１問題を解消
@@ -18,10 +19,17 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def new_animal_params
     params.require(:new_animal).permit( :name, :animal_category_id, :kind_name, :size_id, :sex_id, :age, :birth_date, :personality, :prefecture_id, images: [] )
+  end
+
+  def set_animal
+    @animal = Animal.find(params[:id])
   end
 
 end
