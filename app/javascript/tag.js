aad10d7f509f1ad3a04c1edd4a3df640c6d511b7
tomi0420 <kohei.yoritomi@di-v.co.jp>
2021-05-28 16:25:58 +0900

    インクリメンタルサーチの実装

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -1,5 +1,31 @@
 if (location.pathname.match("animals/new")){
   document.addEventListener("DOMContentLoaded", () => {
-    console.log("読み込み完了");
+    const inputElement = document.getElementById("new_animal_kind_name");
+    inputElement.addEventListener("keyup", () => {
+      const keyword = inputElement.value;
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `search/?keyword=${keyword}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        const searchResult = document.getElementById("search-result");
+        searchResult.innerHTML = "";
+        if (XHR.response) {
+          const tagName = XHR.response.keyword;
+          tagName.forEach((tag) => {
+            const childElement = document.createElement("div");
+            childElement.setAttribute("class", "child");
+            childElement.setAttribute("id", tag.id);
+            childElement.innerHTML = tag.kind_name;
+            searchResult.appendChild(childElement);
+            const clickElement = document.getElementById(tag.id);
+            clickElement.addEventListener("click", () => {
+              inputElement.value = clickElement.textContent;
+              clickElement.remove();
+            });
+          });
+        };
+      };
+    });
   });
 };
